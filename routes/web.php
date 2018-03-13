fee78df4cb29e8ebe56396747202c57ce679cefa
danialdo95 <danialdo95@gmail.com>
2018-03-13 12:14:47 +0800

    Basic Routing & controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return '<h1>Hello World</h2>';
+});
+
+Route::get('/About', function () {
+    return view('pages/about');
+});
+*/
+
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+
+
+/*
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return 'This is user '.$name.' with an id of '.$id;
+});
+*/
