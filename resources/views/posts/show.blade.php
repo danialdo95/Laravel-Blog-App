4df01795434b7486ef773eb5dd7775af1492eddd
danialdo95 <danialdo95@gmail.com>
2018-03-22 00:09:36 +0800

    Edit and Delete Data

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="/posts" class="btn btn-info">Back</a>
+<a href="/posts" ><button type="button" class="btn btn-outline-secondary">Back</button></a>
 <br><br>
    <h1>{{$post->title}}</h1> 
    <div>
@@ -9,4 +9,11 @@
    </div>
    <hr>
    <small>Written on {{$post->created_at}}</small>
+   <hr>
+<a href="/posts/{{$post->id}}/edit"><button type="button" class="btn">Edit</button></a>
+
+   {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method','DELETE')}}
+        {{Form::submit('Delete',['class' => 'btn btn-danger'])}}
+   {!!Form::close()!!}
 @endsection
